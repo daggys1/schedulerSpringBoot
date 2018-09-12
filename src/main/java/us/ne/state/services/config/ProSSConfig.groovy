75ad907162574958189f2e9aa6dfd68a7e33f793
Yogananda <yoga.jdeveloper@gmail.com>
2018-09-12 13:30:57 -0500

    some rest assured

--- src/main/java/us/ne/state/services/config/ProSSConfig.groovy
+++ src/main/java/us/ne/state/services/config/ProSSConfig.groovy
@@ -6,9 +6,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration
 import org.springframework.boot.builder.SpringApplicationBuilder
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
+import org.springframework.scheduling.annotation.EnableScheduling
 
 @SpringBootApplication
 @EnableAutoConfiguration(exclude = ErrorMvcAutoConfiguration)
+@EnableScheduling
 class ProSSConfig extends SpringBootServletInitializer {
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
