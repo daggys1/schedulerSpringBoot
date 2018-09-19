4b5f68864aa0e9a80c0333f9c904742d2323a8c1
Yogananda <yoga.jdeveloper@gmail.com>
2018-09-19 15:09:04 -0500

    no message

--- bin/main/us/ne/state/services/Exceptions/UnauthorizedUserException.groovy
+++
@@ -1,7 +0,0 @@
-package us.ne.state.services.Exceptions
-
-class UnauthorizedUserException extends RuntimeException {
-    UnauthorizedUserException(String var1) {
-        super(var1)
-    }
-}
--- src/test/groovy/us/ne/state/services/controller/SwApiTest.groovy
+++ src/test/groovy/us/ne/state/services/controller/SwApiTest.groovy
@@ -1,33 +1,49 @@
 package us.ne.state.services.controller
 
 import io.restassured.RestAssured
+import io.restassured.config.HttpClientConfig
+import io.restassured.config.RestAssuredConfig
+import io.restassured.http.ContentType
 import io.restassured.path.json.JsonPath
 import io.restassured.response.Response
 import org.junit.Assert
 import org.junit.BeforeClass
 import org.junit.Test
+import org.springframework.web.client.ResourceAccessException
+import static com.jayway.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath
+import static org.hamcrest.Matchers.equalTo
 
 import static io.restassured.RestAssured.given
 
 class SwApiTest {
+    static private RestAssuredConfig config
 
     @BeforeClass
     static void init() {
         RestAssured.baseURI = 'https://swapi.co/api/people'
+        config = RestAssured.config()
+                .httpClient(HttpClientConfig.httpClientConfig()
+                .setParam("http.connection.timeout", 1000)
+                .setParam("http.socket.timeout", 1000))
     }
 
     @Test
     void getLuke() throws Exception {
-        given()
-                .get('/1').then().statusCode(200)
-                .log()
-                .all()
-                .statusCode(200)
+        try {
+            given().config(config)
+                    .get('/1').then()
+                    .log()
+                    .all()
+                    .statusCode(200)
+        } catch (SocketTimeoutException e) {
+            println "SWAPI is DOWN $e.message"
+            throw new ResourceAccessException(e.message)
+        }
+
     }
 
     /**
-     * {
-     *     "name": "Luke Skywalker",
+     *{*     "name": "Luke Skywalker",
      *     "height": "172",
      *     "mass": "77",
      *     "hair_color": "blond",
@@ -57,10 +73,11 @@ class SwApiTest {
      *     "created": "2014-12-09T13:50:51.644000Z",
      *     "edited": "2014-12-20T21:17:56.891000Z",
      *     "url": "https://swapi.co/api/people/1/"
-     * }
-     */
+     *}*/
+
     @Test
     void verifyLuke() {
+        //behavior-driven development (BDD)
         Response response = given().get('/1')
         Assert.assertEquals(200, response.statusCode())
         println response.asString()
@@ -68,6 +85,7 @@ class SwApiTest {
 
         JsonPath jsonPath = new JsonPath(response.asString())
         println jsonPath.get('url')
+
         Assert.assertEquals('Luke Skywalker', jsonPath.get('name'))
         Assert.assertEquals('172', jsonPath.get('height'))
         Assert.assertEquals('77', jsonPath.get('mass'))
@@ -77,10 +95,23 @@ class SwApiTest {
         Assert.assertEquals('19BBY', jsonPath.get('birth_year'))
         Assert.assertEquals('male', jsonPath.get('gender'))
         Assert.assertEquals('https://swapi.co/api/planets/1/', jsonPath.get('homeworld'))
-        Assert.assertEquals(5,jsonPath.getList('films').size())
-        Assert.assertEquals(1,jsonPath.getList('species').size())
-        Assert.assertEquals(2,jsonPath.getList('vehicles').size())
-        Assert.assertEquals(2,jsonPath.getList('starships').size())
+        Assert.assertEquals(5, jsonPath.getList('films').size())
+        Assert.assertEquals(1, jsonPath.getList('species').size())
+        Assert.assertEquals(2, jsonPath.getList('vehicles').size())
+        Assert.assertEquals(2, jsonPath.getList('starships').size())
+    }
 
+
+    @Test
+    void testJsonOfLuke() throws Exception {
+        given().get('/1')
+        .then()
+        .body(matchesJsonSchemaInClasspath('luke.json')).log().all(true)
+        .body("name",equalTo('Luke Skywalker'))
+    }
+
+    @Test
+    void oneANotFound(){
+        given().get('/1a').then().log().all().statusCode(404).body("detail",equalTo('Not found')).contentType(ContentType.JSON)
     }
 }
